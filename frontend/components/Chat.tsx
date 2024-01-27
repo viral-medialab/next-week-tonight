d148584a32635959857dee275ee5e54bdbaee758
valkwek <valeriekwek3@gmail.com>
2024-01-27 11:19:53 -0800

    Displayed Chat Articles

--- frontend/components/Chat.tsx
+++ frontend/components/Chat.tsx
@@ -68,107 +68,123 @@ export default function Chat({ topic, currentArticle, allArticles }: any) {
 
     return (
         currentArticle && (
-                <div className="flex flex-col p-4 border-t">
-                    <div className="flex flex-col items-start w-full ">
-                        <span className="text-gray-500">Try asking:</span>
-                        {currentArticle ? (
-                            /*
-                            CODE TO ADD LINKED TEXT BUTTONS WITH GENERATED QUERIES FROM api/questions
-                            <>
-                              {queries.map((query, index) => (
-                                <button
-                                  key={index}
-                                  className="text-blue-900 mb-1 text-left"
-                                  onClick={() => handleAskQuestion(query)}
-                                >
-                                  {query}
-                                </button>
-                              ))}
-                            </>
-                            */
-                            <>
-                                <button
-                                    className="text-blue-900 mb-1 text-left "
-                                    onClick={() =>
-                                        handleAskQuestion(
-                                            "Question 1" // Summarize the article I am currently reading.
-                                        )
-                                    }
-                                >
-                                    Question 1
-                                </button>
-                                <button
-                                    className="text-blue-900 mb-1 text-left"
-                                    onClick={() =>
-                                        handleAskQuestion(
-                                            "Question 2" // What is unique about the article I am reading?
-                                        )
-                                    }
-                                >
-                                    Question 2
-                                </button>
-                                <button
-                                    className="text-blue-900 mb-1 text-left"
-                                    onClick={() =>
-                                        handleAskQuestion(
-                                            "Question 3" // Translate the first sentence of the article I am reading into spanish
-                                        )
-                                    }
-                                >
-                                    Question 3
-                                </button>
-                            </>
-                        ) : null}
-                    </div>
-
-                    <div className="flex items-center mt-4 w-full">
-                        <textarea
-                            placeholder="Type your message here"
-                            className="flex-1 border rounded-lg py-2 px-4 mr-4 resize-none"
-                            id="message-box"
-                            value={newMessage}
-                            onChange={handleNewMessageChange}
-                            onKeyDown={handleKeyDown}
-                        />
+            <div className="flex flex-col p-4 border-t">
+                <div className="flex flex-col items-start w-full ">
+                    <span className="text-gray-500">Try asking:</span>
+                    {currentArticle ? (
+                        /*
+                        CODE TO ADD LINKED TEXT BUTTONS WITH GENERATED QUERIES FROM api/questions
+                        <>
+                          {queries.map((query, index) => (
+                            <button
+                              key={index}
+                              className="text-blue-900 mb-1 text-left"
+                              onClick={() => handleAskQuestion(query)}
+                            >
+                              {query}
+                            </button>
+                          ))}
+                        </>
+                        */
+                        <>
                         <button
-                            className="bg-gray-500 text-white rounded-full p-2 hover:bg-gray-600"
-                            onClick={handleSendMessage}
-                            disabled={isSubmitting}
+                            className="text-blue-900 mb-1 text-left "
+                            onClick={() =>
+                                handleAskQuestion(
+                                    "Question 1" // Summarize the article I am currently reading.
+                                )
+                            }
                         >
-                            {isSubmitting ? (
-                                "Loading..."
-                            ) : (
-                                <FontAwesomeIcon
-                                    icon={faPaperPlane}
-                                    size="sm"
-                                    className="h-6 w-6"
-                                />
-                            )}
+                            Question 1
                         </button>
-                    </div>
-                    <div className="flex flex-col h-full">
-                        <div className="flex-1 p-4 overflow-y-auto">
-                            {messages.map((message) => (
+                        <button
+                            className="text-blue-900 mb-1 text-left"
+                            onClick={() =>
+                                handleAskQuestion(
+                                    "Question 2" // What is unique about the article I am reading?
+                                )
+                            }
+                        >
+                            Question 2
+                        </button>
+                        <button
+                            className="text-blue-900 mb-1 text-left"
+                            onClick={() =>
+                                handleAskQuestion(
+                                    "Question 3" // Translate the first sentence of the article I am reading into spanish
+                                )
+                            }
+                        >
+                            Question 3
+                        </button>
+                    </>
+                    ) : null}
+                </div>
+
+                <div className="flex items-center mt-4 w-full">
+                    <textarea
+                        placeholder="Type your message here"
+                        className="flex-1 border rounded-lg py-2 px-4 mr-4 resize-none"
+                        id="message-box"
+                        value={newMessage}
+                        onChange={handleNewMessageChange}
+                        onKeyDown={handleKeyDown}
+                    />
+                    <button
+                        className="bg-gray-500 text-white rounded-full p-2 hover:bg-gray-600"
+                        onClick={handleSendMessage}
+                        disabled={isSubmitting}
+                    >
+                        {isSubmitting ? (
+                            "Loading..."
+                        ) : (
+                            <FontAwesomeIcon
+                                icon={faPaperPlane}
+                                size="sm"
+                                className="h-6 w-6"
+                            />
+                        )}
+                    </button>
+                </div>
+
+                <div className="flex flex-col h-full">
+                    <div className="flex-1 p-4 overflow-y-auto">
+                        {messages.map((message) => (
+                            <div
+                                key={message.id}
+                                className={`${
+                                    message.id % 2 === 0
+                                        ? "justify-end"
+                                        : "justify-start"
+                                } flex mb-4`}
+                            >
                                 <div
-                                    key={message.id}
                                     className={`${
                                         message.id % 2 === 0
-                                            ? "justify-end"
-                                            : "justify-start"
-                                    } flex mb-4`}
+                                            ? "bg-gray-600 w-full"
+                                            : "bg-blue-500 w-full"
+                                    } rounded-lg p-2`}
                                 >
-                                    <div
-                                        className={`${
-                                            message.id % 2 === 0
-                                                ? "bg-blue-500"
-                                                : "bg-gray-600"
-                                        } rounded-lg p-2 max-w-xs`}
-                                    >
+                                    {message.id % 2 !== 0 ? (
                                         <p className="text-white">{message.text}</p>
-                                    </div>
+                                    ) : (
+                                        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-8 w-full">
+                                            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                                                <div className="p-6">
+                                                    <h2 className="text-2xl font-bold mb-2">
+                                                        {message.text["title"]}
+                                                    </h2>
+                                                    <p className="text-gray-700 text-base">
+                                                        {message.text["body"]}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         )
@@ -217,26 +233,6 @@ async function submitMessages(
     allArticles: any[],
     messages: { id: number; text: string }[]
 ) {
-//         const res = await fetch('http://localhost:5000/api/call_q2a_workflow', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({ article_id: "BB1hgzNq", user_prompt: "What happens if immigration becomes a key issue in the election?" }),
-//         })
-//         .then(response => response.json())
-//         .then(data => console.log("Function result:", data))
-//         .catch(error => console.error('Error:', error));
-        // To call the function and handle the result
-//         const res = callQ2AWorkflow().then(data => {
-//                       if (data) {
-//                         // Do something with the data here
-//                         console.log("Received data:", data);
-//                       }
-//                       else {
-//                         console.log("No Data");
-//                       }
-//                     });
 //     const res = await fetch("/api/chat", {
 //         method: "POST",
 //         body: JSON.stringify({
@@ -246,13 +242,16 @@ async function submitMessages(
 //             topicName: topic,
 //         }),
 //     });
+//     return res.text();
     const res = await fetch("http://127.0.0.1:5000/api/call_q2a_workflow", {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ article_id: "BB1hgzNq", user_prompt: "What happens if immigration becomes a key issue in the election?" , verbose: true}),
-    }).then((response) => response.json()).then(response => console.log("Function result:", response));
-    return res;
-    // return res.text();
+    });
+    console.log("Raw Response:", res);
+    const data = await res.json();
+    console.log("Function result:", data);
+    return data;
 }
 function convertMessagesToAPIFormat(messages: { id: number; text: string }[]) {
     return messages.map((message) => ({
